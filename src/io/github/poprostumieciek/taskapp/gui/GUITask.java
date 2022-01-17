45540c9851dc1adbcf2bf4ee5627d58440702116
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-17 17:35:35 +0100

    feat: Add focus and select features to `GUITask`

--- src/io/github/poprostumieciek/taskapp/gui/GUITask.java
+++ src/io/github/poprostumieciek/taskapp/gui/GUITask.java
@@ -1,23 +1,41 @@
 package io.github.poprostumieciek.taskapp.gui;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
 import io.github.poprostumieciek.taskapp.tasks.Task;
 
-public class GUITask extends JPanel {
+public class GUITask extends JPanel implements MouseInputListener, ActionListener {
 
-    Task task;
-    JCheckBox checkBox;
-    JLabel taskTitle;
+    private JCheckBox checkBox;
+    private JLabel taskTitle;
+    private Task task;
+    private int idx;
+    private TaskListener taskListener;
 
-    public GUITask(Task task) {
-        this.task = task;
+    private boolean isFocused = false;
+    private boolean isSelected = false;
+
+    public GUITask(Task task, int idx, TaskListener taskListener) {
         this.checkBox = new JCheckBox();
         this.taskTitle = new JLabel();
+
+        this.task = task;
+        this.idx = idx;
+        this.taskListener = taskListener;
 
         this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
         this.add(checkBox);
         this.add(taskTitle);
+
+        this.addMouseListener(this);
+
+        this.checkBox.addActionListener(this);
 
         update();
     }
@@ -25,5 +43,80 @@ public class GUITask extends JPanel {
     void update() {
         taskTitle.setText(task.toString());
         checkBox.setSelected(task.isDone());
+    }
+
+    void setFocused(boolean selected) {
+        this.isFocused = selected;
+        updateBackground();
+    }
+
+    boolean getFocused() {
+        return this.isFocused;
+    }
+
+    void setSelected(boolean selected) {
+        this.isSelected = selected;
+        updateBackground();
+    }
+
+    boolean getSelected() {
+        return this.isSelected;
+    }
+
+    void updateBackground() {
+        if (this.isSelected) {
+            this.setBackground(Color.DARK_GRAY);
+        } else if (this.isFocused) {
+            this.setBackground(Color.GRAY);
+        } else {
+            this.setBackground(UIManager.getColor("Panel.background"));
+        }
+        this.checkBox.setBackground(this.getBackground());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        taskListener.onTaskSelected(this.idx);
+        updateBackground();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        this.setFocused(true);
+        updateBackground();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        this.setFocused(false);
+        updateBackground();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == checkBox) {
+            if (this.checkBox.isSelected()) {
+                this.task.check();
+            } else {
+                this.task.uncheck();
+            }
+        }
+        update();
     }
 }
